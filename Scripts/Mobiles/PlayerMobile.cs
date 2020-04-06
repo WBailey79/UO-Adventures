2c51555d80a4014c2cb23222e3346d06d4b35cc9
Will <williambailey79@gmail.com>
2020-04-06 00:08:27 -0400

    Added LifeStone System

--- Scripts/Mobiles/PlayerMobile.cs
+++ Scripts/Mobiles/PlayerMobile.cs
@@ -3671,6 +3671,19 @@ namespace Server.Mobiles
 
 			base.OnDeath(c);
 
+			#region Life Stone Edit Begin
+            for (int i = 0; i < LifeStoneCore.LifeStoneList.Count; i++)
+            {
+                if (LifeStoneCore.LifeStoneList[i].LifeStoneOwner == this)
+                {
+                    if (LifeStoneCore.LifeStoneList[i].Stone.UsesRemaining > 0)
+                    {
+                        this.SendGump(new LifeStoneReturnGump(this, LifeStoneCore.LifeStoneList[i].Stone));
+                    }
+                }
+            }
+            #endregion Life Stone Edit End
+
 			m_EquipSnapshot = null;
 
 			HueMod = -1;
